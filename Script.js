38f785d25311abfeef883ce76dc150e6aaabcf1a
BhawandeepChahal <46692844+BhawandeepChahal@users.noreply.github.com>
2019-01-14 16:55:20 -0500

    WEEK 2-CLASS HAND IN WORK

---
+++ Script.js
@@ -0,0 +1,16 @@
+// Nmae: Bhawandeep Kaur
+//Date: January 14,2019
+//Student ID: c0734810
+//Course Code: CSD 2214 Section 6
+
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[name="endDate"]');
+
+const clock = document.querySelector('#clock');
+const daysSpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const minutesSpan = clock.querySelector('.minutes');
+const secondsSpan = clock.querySelector('.seconds');
+
+//console.log(endDate.value);
